b92ad4448597a41b3aaacca23675892d9b4d38ae
zhangmeihui <zhangmeihui@cvtt.cn>
2019-10-10 09:41:40 +0800

    index页面添加网易云信二维码功能

--- src/main/java/cn/cvtt/nuoche/common/Constant.java
+++ src/main/java/cn/cvtt/nuoche/common/Constant.java
@@ -8,13 +8,6 @@ package cn.cvtt.nuoche.common;
  */
 public class Constant {
     public static final String TOKEN = "77889cvtt";
-    //yechangmin@cvtt.cn公众号
-//    public static final String APP_ID = "wxda98f445dcbd711a";
-//    public static final String APP_SECRET = "62f4c67b484b422187c53238dd3d3dd2";
-//cuiguoying@cvtt.cn公众号
-//    public static final String APP_ID = "wx9151b43f4906eefc";
-//    public static final String APP_SECRET = "a03894d8afb3d19aaef944b436828609";
-//    public  static  final String  userInfoUrl="https://api.weixin.qq.com/cgi-bin/user/info";
     public static final String APP_ID = "wx7f97a3e9e9cae2d3";
     public static final String APP_SECRET = "8bb628185dc26ddd460901d94e73bc56";
     public  static  final String  userInfoUrl="https://api.weixin.qq.com/cgi-bin/user/info";
@@ -27,18 +20,6 @@ public class Constant {
     public static final String DATETEMPLATE="%d年%d月%d日";
 
 
-
-
-    /**  wexin pay  constant  start*/
-    public  static  final String  MCH_ID="1398897702"; //商户号
-    public  static  final String  NOTIFY_URL="http://i.77889.com/paySuccess";
-    public  static  final String  TRADE_TYPE="JSAPI";
-    public  static  final String  ATTACH="天舟通信海牛助手支付";
-    public  static  final String  SENDBODY="天舟-海牛助手";
-    public  static  final String  NONCE_STR="tianzhoutongxingCommunication";
-    public  static  final String  KEY="sjzljr7571jdjjjxjjgjg14881186111";
-    public  static  final String  ORDERURL="https://api.mch.weixin.qq.com/pay/unifiedorder";
-    /**  end  */
 
 
     /***================================================*/
